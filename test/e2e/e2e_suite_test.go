4494fa4ad03d8346e31ac7d303d3a643d6740be7
Michael Shitrit <mshitrit@redhat.com>
2026-02-01 13:39:54 +0200

    [UNDO] run e2e locally

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -20,6 +20,7 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"os"
 	"testing"
 
 	. "github.com/onsi/ginkgo/v2"
@@ -60,6 +61,8 @@ func TestE2E(t *testing.T) {
 
 var skipall = false
 var _ = BeforeSuite(func() {
+	_ = os.Setenv("KUBECONFIG", "/home/mshitrit/scripts/openshift-installer/openshift-install-4.20/auth/kubeconfig")
+
 	// Skip slow tests if requested
 
 	// Check cluster connection first - skip entire suite if not available
